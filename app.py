2d5940477fa92b2973298c3cd4797be825583a04
lawrenceChege <lawrencechege.s@gmail.com>
2018-06-26 15:41:05 +0300

    add method for handling requests

--- app.py
+++ app.py
@@ -35,3 +35,30 @@ def start():
 	    print "[*] Nothing more buddy..."
 	    sys.exit(1)
     s.close()
+
+def conn_string(conn, data, addr):
+    #client browser request appears here
+    try:
+	first_line = data.split('\n')[0]
+	url = first_line.split(' ')[1]
+	htt_pos = url.find("://")
+	if (http_pos == -1):
+	    temp  url
+	else:
+	    temp = url[(http_pos+3):] #get the rest of the url
+	port_pos = temp.find(":") #find the ps of the port
+	webserver_pos = temp.find("/") #find te end of the webserver
+	if webserver_pos == -1:
+	    webserver_pos = len(temp)
+	webserver = ""
+	port = -1
+	if (port_pos == -1 or webserver_pos < port_pos):
+	    port = 80
+	    webserver = temp[:webserver_pos]
+	else:
+	    #specific port
+	    port = nt((temp[(port_pos+1):])[:webserver_pos -port_pos-1])
+	    webserver = temp[:port_pos]
+	proxy_server(webserver, port, conn, addr, data)
+    except Exeception, e:
+	print (e)
